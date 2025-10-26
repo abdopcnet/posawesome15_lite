b74badfd5049bd0c1c1ec7ed4616d2e9977a6771
abdopcnet <abdopcnet@gmail.com>
2025-10-27 02:21:01 +0300

    💄 Invoice.vue

--- posawesome/public/js/posapp/components/pos/Invoice.vue
+++ posawesome/public/js/posapp/components/pos/Invoice.vue
@@ -181,7 +181,7 @@
       <div class="action-buttons">
         <button class="action-btn primary-btn" :disabled="!hasItems" @click="printInvoice" :title="__('Print invoice')">
           <i class="mdi mdi-printer action-icon"></i>
-          <span>{{ (() => { console.log('🔍 Print button render, $translationTrigger:', $translationTrigger); return $translationTrigger ? __("Print") : "Print"; })() }}</span>
+          <span>{{ __("Print") }}</span>
         </button>
 
         <button class="action-btn success-btn" :disabled="!hasItems || is_payment || isUpdatingTotals"
