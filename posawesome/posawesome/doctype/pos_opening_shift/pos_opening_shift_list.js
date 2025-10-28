e8ab332f7bf4b5bd15c538751f7e40ac3f751219
abdopcnet <abdopcnet@gmail.com>
2025-10-28 04:49:09 +0300

    🔄 pos_opening_shift_list.js

--- posawesome/posawesome/doctype/pos_opening_shift/pos_opening_shift_list.js
+++ posawesome/posawesome/doctype/pos_opening_shift/pos_opening_shift_list.js
@@ -3,14 +3,13 @@
 
 // render
 frappe.listview_settings['POS Opening Shift'] = {
-	get_indicator: function(doc) {
-		var status_color = {
-			"Draft": "grey",
-			"Open": "orange",
-			"Closed": "green",
-			"Cancelled": "red"
-
-		};
-		return [__(doc.status), status_color[doc.status], "status,=,"+doc.status];
-	}
+  get_indicator: function (doc) {
+    var status_color = {
+      Draft: 'grey',
+      Open: 'orange',
+      Closed: 'green',
+      Cancelled: 'red',
+    };
+    return [__(doc.status), status_color[doc.status], 'status,=,' + doc.status];
+  },
 };
