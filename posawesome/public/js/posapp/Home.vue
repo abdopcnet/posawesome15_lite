0b8484c3e8890611fccb0ef72c53bca4b3adbbb1
abdopcnet <abdopcnet@gmail.com>
2025-10-28 04:49:10 +0300

    💄 Home.vue

--- posawesome/public/js/posapp/Home.vue
+++ posawesome/public/js/posapp/Home.vue
@@ -1,15 +1,13 @@
 <!-- @ngrie -->
 <template>
-  <div style="
-    font-size: 0.8rem">
+  <div style="font-size: 0.8rem">
     <main>
       <Navbar @changePage="setPage" />
       <keep-alive>
-        <component :is="page" style="
-          margin: 0 auto;
-          max-width: 100%;
-          width: 100%;
-          min-height: calc(100vh - 32px)" />
+        <component
+          :is="page"
+          style="margin: 0 auto; max-width: 100%; width: 100%; min-height: calc(100vh - 32px)"
+        />
       </keep-alive>
     </main>
   </div>
