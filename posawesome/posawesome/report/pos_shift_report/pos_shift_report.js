a4add54ffa3549d0cad9596caf296d89d2a1094c
abdopcnet <abdopcnet@gmail.com>
2025-11-13 06:18:50 +0300

    🔄 pos_shift_report.js

--- posawesome/posawesome/report/pos_shift_report/pos_shift_report.js
+++ posawesome/posawesome/report/pos_shift_report/pos_shift_report.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2025, future-support and contributors
 // For license information, please see license.txt
 
-frappe.query_reports['POS Shift Report'] = {
+frappe.query_reports['POS_Shift_Report'] = {
   filters: [
     {
       fieldname: 'user',
