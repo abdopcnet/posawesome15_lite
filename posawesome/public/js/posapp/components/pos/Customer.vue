afb8190b30fa7056bb43f573d7f4eb18b678c003
abdopcnet <abdopcnet@gmail.com>
2025-11-18 09:30:10 +0300

    💄 Customer.vue

--- posawesome/public/js/posapp/components/pos/Customer.vue
+++ posawesome/public/js/posapp/components/pos/Customer.vue
@@ -336,6 +336,7 @@
 
 <script>
 import comp from "./Customer.js";
+import { posawesome_logger } from "../../logger.js";
 
 comp.methods = {
   ...comp.methods,
@@ -382,7 +383,7 @@ comp.methods = {
       el.style.color = "#0b66d1";
       el.style.boxShadow = "0 8px 22px rgba(11,102,209,0.12)";
     } catch (err) {
-      console.log("[Customer.vue] buttonHoverEnter error:", err);
+      posawesome_logger.error("Customer.vue", "buttonHoverEnter error", err);
     }
   },
 
@@ -397,7 +398,7 @@ comp.methods = {
       delete el.dataset._origColor;
       delete el.dataset._origBox;
     } catch (err) {
-      console.log("[Customer.vue] buttonHoverLeave error:", err);
+      posawesome_logger.error("Customer.vue", "buttonHoverLeave error", err);
     }
   },
 };
