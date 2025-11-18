4443dfb7ecd91c811402583ccf6f2cb4bed8353b
abdopcnet <abdopcnet@gmail.com>
2025-11-18 12:02:28 +0300

    🔄 pos_closing_shift.js

--- posawesome/posawesome/doctype/pos_closing_shift/pos_closing_shift.js
+++ posawesome/posawesome/doctype/pos_closing_shift/pos_closing_shift.js
@@ -1,26 +1,27 @@
 // Copyright (c) 2020, Youssef Restom and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('POS Closing Shift', {
+frappe.ui.form.on("POS Closing Shift", {
   onload: function (frm) {
-    frm.set_query('pos_profile', function (doc) {
+    frm.set_query("pos_profile", function (doc) {
       return {
         filters: { user: doc.user },
       };
     });
 
-    frm.set_query('user', function (doc) {
+    frm.set_query("user", function (doc) {
       return {
-        query: 'posawesome.posawesome.doctype.pos_closing_shift.pos_closing_shift.get_cashiers',
+        query: "posawesome.api.pos_closing_shift.get_cashiers",
         filters: { parent: doc.pos_profile },
       };
     });
 
-    frm.set_query('pos_opening_shift', function (doc) {
-      return { filters: { status: 'Open', docstatus: 1 } };
+    frm.set_query("pos_opening_shift", function (doc) {
+      return { filters: { status: "Open", docstatus: 1 } };
     });
 
-    if (frm.doc.docstatus === 0) frm.set_value('period_end_date', frappe.datetime.now_datetime());
+    if (frm.doc.docstatus === 0)
+      frm.set_value("period_end_date", frappe.datetime.now_datetime());
     if (frm.doc.docstatus === 1) set_html_data(frm);
   },
 
@@ -28,19 +29,19 @@ frappe.ui.form.on('POS Closing Shift', {
     if (frm.doc.pos_opening_shift && frm.doc.user) {
       reset_values(frm);
       frappe.run_serially([
-        () => frm.trigger('set_opening_amounts'),
-        () => frm.trigger('get_pos_invoices'),
-        () => frm.trigger('get_pos_payments'),
+        () => frm.trigger("set_opening_amounts"),
+        () => frm.trigger("get_pos_invoices"),
+        () => frm.trigger("get_pos_payments"),
       ]);
     }
   },
 
   set_opening_amounts(frm) {
     frappe.db
-      .get_doc('POS Opening Shift', frm.doc.pos_opening_shift)
+      .get_doc("POS Opening Shift", frm.doc.pos_opening_shift)
       .then(({ balance_details }) => {
         balance_details.forEach((detail) => {
-          frm.add_child('payment_reconciliation', {
+          frm.add_child("payment_reconciliation", {
             mode_of_payment: detail.mode_of_payment,
             opening_amount: detail.amount || 0,
             expected_amount: detail.amount || 0,
@@ -51,7 +52,7 @@ frappe.ui.form.on('POS Closing Shift', {
 
   get_pos_invoices(frm) {
     frappe.call({
-      method: 'posawesome.posawesome.doctype.pos_closing_shift.pos_closing_shift.get_pos_invoices',
+      method: "posawesome.api.pos_closing_shift.get_pos_invoices",
       args: {
         pos_opening_shift: frm.doc.pos_opening_shift,
       },
@@ -66,8 +67,7 @@ frappe.ui.form.on('POS Closing Shift', {
 
   get_pos_payments(frm) {
     frappe.call({
-      method:
-        'posawesome.posawesome.doctype.pos_closing_shift.pos_closing_shift.get_payments_entries',
+      method: "posawesome.api.pos_closing_shift.get_payments_entries",
       args: {
         pos_opening_shift: frm.doc.pos_opening_shift,
       },
@@ -81,10 +81,15 @@ frappe.ui.form.on('POS Closing Shift', {
   },
 });
 
-frappe.ui.form.on('POS Closing Shift Detail', {
+frappe.ui.form.on("POS Closing Shift Detail", {
   closing_amount: (frm, cdt, cdn) => {
     const row = locals[cdt][cdn];
-    frappe.model.set_value(cdt, cdn, 'difference', flt(row.expected_amount - row.closing_amount));
+    frappe.model.set_value(
+      cdt,
+      cdn,
+      "difference",
+      flt(row.expected_amount - row.closing_amount)
+    );
   },
 });
 
@@ -107,7 +112,7 @@ function set_form_payments_data(data, frm) {
 }
 
 function add_to_pos_transaction(d, frm) {
-  frm.add_child('pos_transactions', {
+  frm.add_child("pos_transactions", {
     sales_invoice: d.name,
     posting_date: d.posting_date,
     grand_total: d.grand_total,
@@ -116,7 +121,7 @@ function add_to_pos_transaction(d, frm) {
 }
 
 function add_to_pos_payments(d, frm) {
-  frm.add_child('pos_payments', {
+  frm.add_child("pos_payments", {
     payment_entry: d.name,
     posting_date: d.posting_date,
     paid_amount: d.paid_amount,
@@ -128,24 +133,24 @@ function add_to_pos_payments(d, frm) {
 function add_to_payments(d, frm) {
   d.payments.forEach((p) => {
     const payment = frm.doc.payment_reconciliation.find(
-      (pay) => pay.mode_of_payment === p.mode_of_payment,
+      (pay) => pay.mode_of_payment === p.mode_of_payment
     );
     if (payment) {
       let amount = p.amount;
       let cash_mode_of_payment = get_value(
-        'POS Profile',
+        "POS Profile",
         frm.doc.pos_profile,
-        'posa_cash_mode_of_payment',
+        "posa_cash_mode_of_payment"
       );
       if (!cash_mode_of_payment) {
-        cash_mode_of_payment = 'Cash';
+        cash_mode_of_payment = "Cash";
       }
       if (payment.mode_of_payment == cash_mode_of_payment) {
         amount = p.amount - d.change_amount;
       }
       payment.expected_amount += flt(amount);
     } else {
-      frm.add_child('payment_reconciliation', {
+      frm.add_child("payment_reconciliation", {
         mode_of_payment: p.mode_of_payment,
         opening_amount: 0,
         expected_amount: p.amount || 0,
@@ -156,13 +161,13 @@ function add_to_payments(d, frm) {
 
 function add_pos_payment_to_payments(p, frm) {
   const payment = frm.doc.payment_reconciliation.find(
-    (pay) => pay.mode_of_payment === p.mode_of_payment,
+    (pay) => pay.mode_of_payment === p.mode_of_payment
   );
   if (payment) {
     let amount = p.paid_amount;
     payment.expected_amount += flt(amount);
   } else {
-    frm.add_child('payment_reconciliation', {
+    frm.add_child("payment_reconciliation", {
       mode_of_payment: p.mode_of_payment,
       opening_amount: 0,
       expected_amount: p.amount || 0,
@@ -173,12 +178,12 @@ function add_pos_payment_to_payments(p, frm) {
 function add_to_taxes(d, frm) {
   d.taxes.forEach((t) => {
     const tax = frm.doc.taxes.find(
-      (tx) => tx.account_head === t.account_head && tx.rate === t.rate,
+      (tx) => tx.account_head === t.account_head && tx.rate === t.rate
     );
     if (tax) {
       tax.amount += flt(t.tax_amount);
     } else {
-      frm.add_child('taxes', {
+      frm.add_child("taxes", {
         account_head: t.account_head,
         rate: t.rate,
         amount: t.tax_amount,
@@ -188,31 +193,31 @@ function add_to_taxes(d, frm) {
 }
 
 function reset_values(frm) {
-  frm.set_value('pos_transactions', []);
-  frm.set_value('payment_reconciliation', []);
-  frm.set_value('pos_payments', []);
-  frm.set_value('taxes', []);
-  frm.set_value('grand_total', 0);
-  frm.set_value('net_total', 0);
-  frm.set_value('total_quantity', 0);
+  frm.set_value("pos_transactions", []);
+  frm.set_value("payment_reconciliation", []);
+  frm.set_value("pos_payments", []);
+  frm.set_value("taxes", []);
+  frm.set_value("grand_total", 0);
+  frm.set_value("net_total", 0);
+  frm.set_value("total_quantity", 0);
 }
 
 function refresh_fields(frm) {
-  frm.refresh_field('pos_transactions');
-  frm.refresh_field('payment_reconciliation');
-  frm.refresh_field('pos_payments');
-  frm.refresh_field('taxes');
-  frm.refresh_field('grand_total');
-  frm.refresh_field('net_total');
-  frm.refresh_field('total_quantity');
+  frm.refresh_field("pos_transactions");
+  frm.refresh_field("payment_reconciliation");
+  frm.refresh_field("pos_payments");
+  frm.refresh_field("taxes");
+  frm.refresh_field("grand_total");
+  frm.refresh_field("net_total");
+  frm.refresh_field("total_quantity");
 }
 
 function set_html_data(frm) {
   frappe.call({
-    method: 'get_payment_reconciliation_details',
+    method: "get_payment_reconciliation_details",
     doc: frm.doc,
     callback: (r) => {
-      frm.get_field('payment_reconciliation_details').$wrapper.html(r.message);
+      frm.get_field("payment_reconciliation_details").$wrapper.html(r.message);
     },
   });
 }
@@ -220,7 +225,7 @@ function set_html_data(frm) {
 const get_value = (doctype, name, field) => {
   let value;
   frappe.call({
-    method: 'frappe.client.get_value',
+    method: "frappe.client.get_value",
     args: {
       doctype: doctype,
       filters: { name: name },
