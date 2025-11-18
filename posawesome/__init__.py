6a72bb15e2124b22c673ac8a2cd6916df842cd97
abdopcnet <abdopcnet@gmail.com>
2025-11-18 09:17:48 +0300

    🐍 __init__.py

--- posawesome/__init__.py
+++ posawesome/__init__.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import logging
 import frappe
+from frappe.utils.logger import get_logger
 
 __version__ = "18.11.2025"
+
+# Backend Logger
+backend_logger = get_logger('posawesome_backend', max_size=1_000_000)
+backend_logger.setLevel(logging.INFO)
+
+# Frontend Logger
+frontend_logger = get_logger('posawesome_frontend', max_size=1_000_000)
+frontend_logger.setLevel(logging.INFO)
 
 
 def console(*data):
