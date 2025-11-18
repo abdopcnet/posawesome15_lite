083ef4c7ac82e46f3adb5228868ddb7d09cf8bad
abdopcnet <abdopcnet@gmail.com>
2025-11-18 09:30:10 +0300

    🔄 OpenShiftsWarning.js

--- posawesome/public/js/posapp/components/pos/OpenShiftsWarning.js
+++ posawesome/public/js/posapp/components/pos/OpenShiftsWarning.js
@@ -1,4 +1,6 @@
 // ===== OPEN SHIFTS WARNING COMPONENT =====
+import { posawesome_logger } from "../../logger.js";
+
 export default {
   name: "OpenShiftsWarning",
 
@@ -29,7 +31,7 @@ export default {
           hour12: true,
         });
       } catch (error) {
-        console.log("[OpenShiftsWarning.js] formatDate error:", error);
+        posawesome_logger.error("[OpenShiftsWarning.js] formatDate error:", error);
         return dateString;
       }
     },
